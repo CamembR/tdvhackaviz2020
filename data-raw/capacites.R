bcd53c2aaee66f212ecf5bea02dfe0190955b47f
romainx <cros.romain@gmail.com>
2020-03-14 22:30:58 +0100

    V0.1.1
    - Capacite
        - all columns to lower case.
        - `dpt` is now `dpt_<num dpt>` to be homogeneous
        - Fix encoding

--- data-raw/capacites.R
+++ data-raw/capacites.R
@@ -1,6 +1,7 @@
 library(readr)
 library(usethis)
 library(stringr)
+library(dplyr)
 
 # Source https://github.com/ToulouseDataViz/Hackaviz2020
 file_name <- "capacites.csv"
@@ -14,11 +15,15 @@ if (!file.exists(raw_data)) {
 
 raw <- readr::read_csv(raw_data,
                        col_names = TRUE,
-                       na = "(NA)"
+                       na = "(NA)",
+                       locale = locale("fr", encoding = "ISO-8859-1")
 )
 
-# No transformation needed
-capacites <- raw
+capacites <- raw %>%
+    rename_all(tolower) %>%
+    rename(num_dpt = dpt) %>%
+    mutate(dpt = str_glue("dpt_{str_pad(num_dpt, 2, side = 'left', pad=0)}")) %>%
+    select("dpt", "num_dpt", "nom_dpt", everything())
 
 write_csv(capacites, raw_data)
 usethis::use_data(capacites, overwrite = TRUE, compress = 'xz')
